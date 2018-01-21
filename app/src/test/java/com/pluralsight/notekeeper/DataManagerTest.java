0f37b8a0ca845034034313454e2b2b2e59874594
ait27ru <ait27ru@gmail.com>
2018-01-21 18:11:19 +1300

    * Added pre-processing at the start of each test class and method.

--- app/src/test/java/com/pluralsight/notekeeper/DataManagerTest.java
+++ app/src/test/java/com/pluralsight/notekeeper/DataManagerTest.java
@@ -1,5 +1,7 @@
 package com.pluralsight.notekeeper;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,24 +10,61 @@ import static org.junit.Assert.*;
  * Created by andrey on 19/01/2018.
  */
 public class DataManagerTest {
+    static DataManager sDataManager;
+
+    @BeforeClass
+    public static void classSetUp() throws Exception {
+        sDataManager = DataManager.getInstance();
+    }
+
+    @Before
+    public void  setUp() throws Exception {
+        sDataManager.getNotes().clear();
+        sDataManager.initializeExampleNotes();
+    }
+
     @Test
     public void createNewNote() throws Exception {
-        DataManager dm = DataManager.getInstance();
-        final CourseInfo course = dm.getCourse("android_async");
+        final CourseInfo course = sDataManager.getCourse("android_async");
         final String noteTitle = "Test note title";
         final String noteText = "Test note text";
 
-        int noteIndex = dm.createNewNote();
-        NoteInfo newNote = dm.getNotes().get(noteIndex);
+        int noteIndex = sDataManager.createNewNote();
+        NoteInfo newNote = sDataManager.getNotes().get(noteIndex);
         newNote.setCourse(course);
         newNote.setTitle(noteTitle);
         newNote.setText(noteText);
 
-        NoteInfo compareNote = dm.getNotes().get(noteIndex);
+        NoteInfo compareNote = sDataManager.getNotes().get(noteIndex);
 
         assertEquals(course, compareNote.getCourse());
         assertEquals(noteTitle, compareNote.getTitle());
         assertEquals(noteText, compareNote.getText());
     }
 
+    @Test
+    public void findSimilarNotes() throws Exception {
+        final CourseInfo course = sDataManager.getCourse("android_async");
+        final String noteTitle = "Test note title";
+        final String noteText1 = "Test note text first";
+        final String noteText2 = "Test note text second";
+
+        int noteIndex1 = sDataManager.createNewNote();
+        NoteInfo newNote1 = sDataManager.getNotes().get(noteIndex1);
+        newNote1.setCourse(course);
+        newNote1.setTitle(noteTitle);
+        newNote1.setText(noteText1);
+
+        int noteIndex2 = sDataManager.createNewNote();
+        NoteInfo newNote2 = sDataManager.getNotes().get(noteIndex2);
+        newNote2.setCourse(course);
+        newNote2.setTitle(noteTitle);
+        newNote2.setText(noteText2);
+
+        int foundIndex1 = sDataManager.findNote(newNote1);
+        assertEquals(noteIndex1, foundIndex1);
+
+        int foundIndex2 = sDataManager.findNote(newNote2);
+        assertEquals(noteIndex2, foundIndex2);
+    }
 }
