43ad4d15f1fda5da96fdc2a21d1fff2efecc90c0
ait27ru <ait27ru@gmail.com>
2018-01-24 21:47:26 +1300

    L2-Ch3 * Added RecyclerView Adapter class

---
+++ app/src/main/java/com/pluralsight/notekeeper/NoteRecyclerAdapter.java
@@ -0,0 +1,52 @@
+package com.pluralsight.notekeeper;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by andrey on 23/01/2018.
+ */
+
+public class NoteRecyclerAdapter extends RecyclerView.Adapter<NoteRecyclerAdapter.ViewHolder> {
+
+    private final Context mContext;
+    private final LayoutInflater mLayoutInflater;
+
+    public NoteRecyclerAdapter(Context context ) {
+        mContext = context;
+        mLayoutInflater = LayoutInflater.from(mContext);
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = mLayoutInflater.inflate(R.layout.item_note_list, parent, false);
+        return new ViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView mTextCourse;
+        public TextView mTextTitle;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            mTextCourse = itemView.findViewById(R.id.text_course);
+            mTextTitle = itemView.findViewById(R.id.text_title);
+        }
+    }
+}
+
