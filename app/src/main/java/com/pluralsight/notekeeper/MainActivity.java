6fe1a0880297ef40d4db999afb02d7ef1d9247dc
ait27ru <ait27ru@gmail.com>
2018-02-07 22:40:26 +1300

    L2-Ch5 * Added Navigation View options

--- app/src/main/java/com/pluralsight/notekeeper/MainActivity.java
+++ app/src/main/java/com/pluralsight/notekeeper/MainActivity.java
@@ -117,22 +117,23 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
+        if (id == R.id.nav_notes) {
+            handleSelection("Notes");
+        } else if (id == R.id.nav_courses) {
+            handleSelection("Courses");
         } else if (id == R.id.nav_share) {
-
+            handleSelection("Share");
         } else if (id == R.id.nav_send) {
-
+            handleSelection("Send");
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void handleSelection(String message) {
+        View view = findViewById(R.id.list_items);
+        Snackbar.make(view, message, Snackbar.LENGTH_LONG).show();
     }
 }
