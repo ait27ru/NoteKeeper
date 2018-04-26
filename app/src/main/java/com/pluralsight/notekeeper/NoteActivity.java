343747cd29321656d452b770ae01a38005acb96b
ait27ru <ait27ru@gmail.com>
2018-04-26 21:26:50 +1200

    L2-ExposeData-Ch2 * Added content provider to NoteKeeper * Implemented content provider over SQLite

--- app/src/main/java/com/pluralsight/notekeeper/NoteActivity.java
+++ app/src/main/java/com/pluralsight/notekeeper/NoteActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -20,6 +21,7 @@ import android.widget.Spinner;
 
 import com.pluralsight.notekeeper.NoteKeeperDatabaseContract.CourseInfoEntry;
 import com.pluralsight.notekeeper.NoteKeeperDatabaseContract.NoteInfoEntry;
+import com.pluralsight.notekeeper.NoteKeeperProviderContract.Courses;
 
 public class NoteActivity extends AppCompatActivity
         implements LoaderManager.LoaderCallbacks<Cursor> {
@@ -333,20 +335,13 @@ public class NoteActivity extends AppCompatActivity
 
     private CursorLoader createLoaderCourses() {
         mCoursesQueryFinished = false;
-        return new CursorLoader(this) {
-            @Override
-            public Cursor loadInBackground() {
-                SQLiteDatabase db = mDbOpenHelper.getReadableDatabase();
-                String[] courseColumns = {
-                        CourseInfoEntry.COLUMN_COURSE_TITLE,
-                        CourseInfoEntry.COLUMN_COURSE_ID,
-                        CourseInfoEntry._ID
-                };
-                return db.query(CourseInfoEntry.TABLE_NAME, courseColumns,
-                        null, null, null, null, CourseInfoEntry.COLUMN_COURSE_TITLE);
-
-            }
+        Uri uri = Courses.CONTENT_URI;
+        String[] courseColumns = {
+                Courses.COLUMN_COURSE_TITLE,
+                Courses.COLUMN_COURSE_ID,
+                Courses._ID
         };
+        return new CursorLoader(this, uri, courseColumns, null, null, Courses.COLUMN_COURSE_TITLE);
     }
 
     private CursorLoader createLoaderNotes() {
@@ -376,7 +371,7 @@ public class NoteActivity extends AppCompatActivity
         if (loader.getId() == LOADER_NOTES) {
             loadFinishedNotes(data);
         } else if (loader.getId() == LOADER_COURSES) {
-            mAdapterCourses.changeCursor(data);
+               mAdapterCourses.changeCursor(data);
             mCoursesQueryFinished = true;
             displayNoteWhenQuerysFinished();
         }
