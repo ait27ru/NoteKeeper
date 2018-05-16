a1649a118ccf3181e6e4e5267d874b007356cd16
ait27ru_ubuntu <ait27ru@gmail.com>
2018-05-16 23:02:25 +1200

    advanced/platform-power/ch-3 * Added JobScheduler and Job for uploading notes in the background.

---
+++ app/src/main/java/com/pluralsight/notekeeper/NoteUploaderJobService.java
@@ -0,0 +1,25 @@
+package com.pluralsight.notekeeper;
+
+import android.app.Service;
+import android.app.job.JobParameters;
+import android.app.job.JobService;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class NoteUploaderJobService extends JobService {
+
+    public static final String EXTRA_DATA_URI = "com.pluralsight.notekeeper.extras.DATA_URI";
+
+    public NoteUploaderJobService() {
+    }
+
+    @Override
+    public boolean onStartJob(JobParameters params) {
+        return false;
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters params) {
+        return false;
+    }
+}
